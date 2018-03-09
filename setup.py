9e8f7f24a2b3c43de36abd23235954cd9443d249
hyrfilm <jonas@daknight.se>
2018-03-09 13:10:32 +0100

    Fixed up dummy setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='skivvy',
-    packages=['skivvy'],  # this must be the same as the name above
+    packages=['skivvy'],
     version='0.1',
     description='A simple tool for testing JSON/HTTP APIs',
     author='Jonas Holmer',
     author_email='jonas.holmer@gmail.com',
-    url='https://github.com/hyrfilm/skivvy',  # use the URL to the github repo
-    download_url='https://github.com/peterldowns/mypackage/archive/0.1.tar.gz',  # I'll explain this in a second
-    keywords=['testing', 'automation', 'unit-testing', 'json', 'API', 'HTTP'],  # arbitrary keywords
+    url='https://github.com/hyrfilm/skivvy',
+    download_url='https://github.com/hyrfilm/mypackage/archive/0.1.tar.gz',
+    keywords=['testing', 'automation', 'unit-testing', 'json', 'API', 'HTTP'],
     classifiers=[],
 )
