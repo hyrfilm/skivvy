dd03a38754acee67b0c8542478bbfe248c61d8b0
jonasholmer <jonas@mindmore.com>
2022-01-05 17:22:31 +0100

    Bump version to 0.18

--- skivvy/skivvy.py
+++ skivvy/skivvy.py
@@ -121,7 +121,7 @@ def dump_response_headers(headers_to_write, r):
 
 
 def run():
-    arguments = docopt(__doc__, version='skivvy 0.17')
+    arguments = docopt(__doc__, version='skivvy 0.18')
     conf = read_config(arguments.get("<cfg_file>"))
     tests = file_util.list_files(conf.tests, conf.ext)
     failures = 0
