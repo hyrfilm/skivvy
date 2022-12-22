8065fe0228e88ad2e63289ea33fabb968ca0c3ec
jonasholmer <jonas@mindmore.com>
2022-12-22 10:57:45 +0100

    Version bump: 0.402

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='skivvy',
     packages=find_packages(exclude=["*.json"]),
-    version='0.401',
+    version='0.402',
     description='A simple tool for testing JSON/HTTP APIs',
     author='Jonas Holmer',
     author_email='jonas.holmer@gmail.com',
