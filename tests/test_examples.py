aaff3f23671eba3fc95651b3db21dbd329bdc102
hyrfilm <jonas.holmer@gmail.com>
2025-12-11 05:21:31 +0100

    Added integration-ish tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,18 +1,22 @@
 import sys
 
 from skivvy.skivvy import run
+from skivvy.util import file_util
 
+
+# TODO: This a ugly and slow and bad and hacky since we're hitting the actual end-points but blabla fix laterrrz
 
 def test_jsonplaceholder_examples():
     cfg_file = "examples/typicode/default.json"
     result = run_examples(cfg_file)
-    assert result, f"Example config {cfg_file} failed with result={result!r}"
+    assert result, f"Example config {cfg_file} failed"
 
-def test_jsonplaceholder_examples():
+def test_dummyjson_examples():
     cfg_file = "examples/dummyjson/dummy.json"
     result = run_examples(cfg_file)
-    assert result, f"Example config {cfg_file} failed with result={result!r}"
-
+    assert result, f"Example config {cfg_file} failed"
+    # god knows why this is needed
+    file_util.cleanup_tmp_files(throw=False)
 
 
 def run_examples(cfg_file):
